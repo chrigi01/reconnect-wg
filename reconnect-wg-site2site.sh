ca18c9ec0b3dc910b6eff4b8114e9cb4e37bf22d
chrigi01 <87994635+chrigi01@users.noreply.github.com>
2022-02-06 16:25:48 +0100

    Update reconnect-wg-site2site.sh
    log files added

--- reconnect-wg-site2site.sh
+++ reconnect-wg-site2site.sh
@@ -6,13 +6,15 @@ PEER_KEY=$2
 PEER_IP=$3
 ENDPOINT=$4
 PORT=$5
+LOG_FILE=/var/log/reconnect-wg-site2site.log
 
-wg show ${INTERFACE}
+date > ${LOG_FILE}
+wg show ${INTERFACE} >> ${LOG_FILE}
 if /bin/ping -c 1 ${PEER_IP} &> /dev/null
 then
-        echo "solid connection to ${ENDPOINT}"
+        echo "solid connection to ${ENDPOINT}" >> ${LOG_FILE}
 else
-        echo "endpoint ${ENDPOINT}:${PORT} will be reconnected"
+        echo "endpoint ${ENDPOINT}:${PORT} will be reconnected" >> ${LOG_FILE}
         wg set ${INTERFACE} peer ${PEER_KEY} endpoint ${ENDPOINT}:${PORT}
-        wg show ${INTERFACE}
+        wg show ${INTERFACE} >> ${LOG_FILE}
 fi
